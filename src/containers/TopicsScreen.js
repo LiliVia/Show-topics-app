6848e97bd658c599057cc11e86905f9a88c74e46
Lili Via <lilivenone@gmail.com>
2018-09-10 00:24:18 +0300

    selectors and reducers, mapStateToProps, ListView

--- src/containers/TopicsScreen.js
+++ src/containers/TopicsScreen.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './TopicsScreen.css';
 import * as topicsActions from '../store/topics/actions';
+import * as topicsSelectors from '../store/topics/reducer';
+import ListView from '../components/ListView';
 
 class TopicsScreen extends Component {
   componentDidMount() {
@@ -9,13 +11,36 @@ class TopicsScreen extends Component {
   }
   render() {
     return (
-      <h2>Where are my topics?</h2>
+      <div className="TopicsScreen">
+        <ListView
+          rowsIdArray={this.props.rowsIdArray}
+          rowsById={this.props.rowsById}
+          renderRow={this.renderRow} />
+      </div>
+    );
+  }
+
+  renderLoading() {
+    return (
+      <p>Loading...</p>
+    );
+  }
+
+  renderRow(row) {
+    return (
+      <div>
+        <h3>{row.title}</h3>
+        <p>{row.description}</p>
+      </div>
     );
   }
 }
 
 function mapStateToProps(state) {
-  return {};
+  return {
+    rowsById: topicsSelectors.getTopicsByUrl(state),
+    rowsIdArray: topicsSelectors.getTopicsUrlArray(state)
+  };
 }
 
 export default connect(mapStateToProps)(TopicsScreen);
--- src/store/topics/reducer.js
+++ src/store/topics/reducer.js
@@ -1,13 +1,29 @@
+import _ from 'lodash';
+import * as types from './actionTypes';
 import Immutable from 'seamless-immutable';
 
 const initialState = Immutable({
-  topicsByUrl: {},
+  topicsByUrl: undefined,
   selectedTopicUrls: []
 });
 
 export default function reduce(state = initialState, action = {}) {
   switch (action.type) {
+    case types.TOPICS_FETCHED:
+      return state.merge({
+        topicsByUrl: action.topicsByUrl
+      });
     default:
       return state;
   }
+}
+
+//selectors
+
+export function getTopicsByUrl(state) {
+  return state.topics.topicsByUrl;
+}
+
+export function getTopicsUrlArray(state) {
+  return _.keys(state.topics.topicsByUrl);
 }
